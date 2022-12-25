8cc58f112719bc6b90042b684f23bd5a6466ff39
JYKANG <xadamantiumx@naver.com>
2022-12-26 01:40:37 +0900

    1226-0140

--- 강지윤/1주차/test.py
+++ 강지윤/1주차/test.py
@@ -1,13 +1,24 @@
-n = int(input()) # 수열의 길이
-a = list(map(int, input().split())) # 수열
-dp = [[a[i]] for i in range(n)] # dp, 초기 수열 설정
-
-for i in range(1, len(a)) :
-    num = a[i]
-    for j in range(0, i) :
-        if a[j] < num and len(dp[i]) <= len(dp[j]): # 증가하는 수열인지 and 최장증가수열인지
-            dp[i] = dp[j] + [num]
-
-answer = max(dp, key = len)
-print(len(answer))
-print(* answer)
+from sys import stdin
+N=int(stdin.readline())
+A=list(map(int,stdin.readline().split()))
+test=[]
+test1=[] #현재기준 LIS
+count=0
+test.append(A[0])
+i=0
+while True:
+    if count==N-1: #전체 다 돌았을 시
+        break
+    if i==N-1: #마지막 원소까지 왔을때
+        if len(test1)>len(test): #test1: 이전까지 LIS , test=현재 부분수열
+            test=[]
+        elif len(test1)<len(test):
+            test1=[]
+            test1=test
+        count+=1
+        i=count-1 #밑에 i+=1 고려함
+    if A[i]<A[i+1] and test[-1]<A[i+1]:
+        test.append(A[i+1])
+    i+=1
+print(len(test1))
+print(*test1)
