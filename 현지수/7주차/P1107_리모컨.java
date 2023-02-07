dc6b54de78a1f754019a7c9c6291b8c23de45800
jisoo <ske050588@gmail.com>
2023-02-07 20:01:06 +0900

    7주차 완전탐색

--- 현지수/7주차/P1107_리모컨.java
+++ 현지수/7주차/P1107_리모컨.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class P1107_리모콘 {
+public class P1107_리모컨 {
     public static boolean[] broken;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
